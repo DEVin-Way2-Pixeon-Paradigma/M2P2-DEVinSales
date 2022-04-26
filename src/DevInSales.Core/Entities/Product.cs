4c75de3408d718dfb893c33af22b78d324f68e0e
Andre Motta <93352462+andreMotta22@users.noreply.github.com>
2022-04-26 18:03:20 -0300

    fazendo validação sobre o preço

--- src/DevInSales.Core/Entities/Product.cs
+++ src/DevInSales.Core/Entities/Product.cs
@@ -4,16 +4,25 @@ namespace DevInSales.Core.Entities
 {
     public class Product : Entity
     {
-        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
-        public string Name { get; private set; }
         
-        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
-        public decimal SuggestedPrice { get; private set; }
 
         public Product(string name, decimal suggestedPrice)
         {
             Name = name;
             SuggestedPrice = suggestedPrice;
         }
+
+        public void AtualizarDados(Product produto) {
+            Name = produto.Name;
+            SuggestedPrice = produto.SuggestedPrice;
+        }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
+        public string Name { get; private set; }
+        
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
+        [Range(1,(double)decimal.MaxValue)]
+        public decimal SuggestedPrice { get; private set; }
     }
 }
