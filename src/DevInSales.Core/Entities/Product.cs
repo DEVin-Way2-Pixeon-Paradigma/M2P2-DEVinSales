89adc491a1530b0394bef316660ad87383b7e7bf
Andre Motta <93352462+andreMotta22@users.noreply.github.com>
2022-04-26 17:12:23 -0300

    usando o modelo de validação nativo

--- src/DevInSales.Core/Entities/Product.cs
+++ src/DevInSales.Core/Entities/Product.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DevInSales.Core.Entities
 {
     public class Product : Entity
     {
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
         public string Name { get; private set; }
+        
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
         public decimal SuggestedPrice { get; private set; }
 
         public Product(string name, decimal suggestedPrice)
