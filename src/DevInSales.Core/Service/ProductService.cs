bb6b6461a0f3be58146715adc314a9f71823d7e6
Andre Motta <93352462+andreMotta22@users.noreply.github.com>
2022-04-26 15:06:25 -0300

    criando a classe de serviço produto

---
+++ src/DevInSales.Core/Service/ProductService.cs
@@ -0,0 +1,20 @@
+using DevInSales.Core.Data.Context;
+using DevInSales.Core.Entities;
+using DevInSales.Core.Interface;
+
+
+namespace DevInSales.Core.Service
+{
+    public class ProductService : IProduct
+    {
+        private readonly DataContext _context;
+
+        public ProductService(DataContext context) {
+            _context = context;
+        }
+        public void Atualizar(Product produto)
+        {
+            
+        }
+    }
+}
